0d7e032d8aabf5551df77fbd8f8553e524726ba6
lazyrabbit <liangxinyjd@163.com>
2019-06-21 14:50:58 +0800

    bootstraptable demo修改

--- rabb-thymleaf-bootstrap/src/main/java/xyz/lazyrabbit/controller/ThymleafController.java
+++ rabb-thymleaf-bootstrap/src/main/java/xyz/lazyrabbit/controller/ThymleafController.java
@@ -2,6 +2,7 @@ package xyz.lazyrabbit.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import xyz.lazyrabbit.model.TableResult;
 import xyz.lazyrabbit.model.TreeNode;
@@ -9,39 +10,44 @@ import xyz.lazyrabbit.model.User;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 public class ThymleafController {
     @GetMapping("/treeview")
-    public String treeview(){
+    public String treeview() {
         return "treeview/index";
     }
+
     @GetMapping("/validator")
-    public String validator(){
+    public String validator() {
         return "validator/index";
     }
+
     @GetMapping("/table")
-    public String table(){
+    public String table() {
         return "table/index";
     }
+
     @GetMapping("/getNodes")
     @ResponseBody
-    public List<TreeNode> getNodes(){
+    public List<TreeNode> getNodes() {
         List<TreeNode> list = new ArrayList<TreeNode>();
         list.add(new TreeNode(999, "无限月读"));
         return list;
     }
+
     @GetMapping("/getUsers")
     @ResponseBody
-    public TableResult<User> getUsers(){
+    public TableResult<User> getUsers(@RequestParam(required = false, value = "offset", defaultValue = "0") Integer offset,
+                                      @RequestParam(required = false, value = "limit", defaultValue = "5") Integer limit,
+                                      @RequestParam(required = false, value = "order", defaultValue = "asc") String order,
+                                      @RequestParam(required = false, value = "sort", defaultValue = "id") String sort,
+                                      @RequestParam(required = false, value = "search", defaultValue = "") String serach) {
         List<User> list = new ArrayList<User>();
-        list.add(new User(1,"Messi","a","1111"));
-        list.add(new User(2,"Lucas","a","22222"));
-        list.add(new User(3,"Neymar","a","333333"));
-        list.add(new User(4,"CR7","a","44444"));
-        list.add(new User(5,"Bale","a","55555"));
-        list.add(new User(6,"Benzema","a","66666"));
-        list.add(new User(6,"Benzema","a","66666"));
-        return new TableResult<>(list.size(),list);
+        for (int i = 0; i < limit; i++) {
+            list.add(new User(offset + i + 1, "Benzema" + serach, serach + "@gmail.com", UUID.randomUUID().toString().replaceAll("-", "")));
+        }
+        return new TableResult<>(list.size() * 10 + 1, list);
     }
 }
