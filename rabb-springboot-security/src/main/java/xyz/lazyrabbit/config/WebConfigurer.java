c5e9b81daf01d1f267d1621e7a234b94565b458b
lazyrabbit <fish@lazyrabbit.xyz>
2020-06-19 17:29:10 +0800

    添加jpa项目

--- rabb-springboot-security/src/main/java/xyz/lazyrabbit/config/WebConfigurer.java
+++ rabb-springboot-security/src/main/java/xyz/lazyrabbit/config/WebConfigurer.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import xyz.lazyrabbit.interceptor.LoginInterceptor;
-import xyz.lazyrabbit.model.User;
 
 @Configuration
 public class WebConfigurer implements WebMvcConfigurer {
