898bbbc3e2f40acf4a30327c6f180dae4cb067b3
lazyrabbit-onfish <fish@lazyrabbit.xyz>
2020-10-21 10:19:55 +0800

    添加redis集成模块

---
+++ rabb-springboot-data-redis/src/main/java/xyz/lazyrabbit/data/redis/config/LettuceRedisConfig.java
@@ -0,0 +1,27 @@
+package xyz.lazyrabbit.data.redis.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.io.Serializable;
+
+@Configuration
+public class LettuceRedisConfig {
+
+    /**
+     * 默认使用JDK自带序列化方式
+     * 这里修改为其他方式
+     */
+    @Bean
+    public RedisTemplate<String, Serializable> redisTemplate(LettuceConnectionFactory connectionFactory) {
+        RedisTemplate<String, Serializable> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setConnectionFactory(connectionFactory);
+        return redisTemplate;
+    }
+}
